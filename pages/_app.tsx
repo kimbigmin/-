de2fc3fccbafde34ff07c7e14a45ec4d4e75bd8d
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-07-17 11:44:43 +0900

    [공통] 디렉토리 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,39 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createStore, applyMiddleware, combineReducers, Store } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import withRedux from 'next-redux-wrapper';
+import thunk from 'redux-thunk';
+import data from '../store/Data';
+import view from '../store/View';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Provider } from 'react-redux';
+import hp from '../core/Hp';
+
+const rootReducer = combineReducers({
+  data,
+  view
+})
+
+const store: Store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Provider store={store}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Provider>
     </>
   )
   
 }
 
-export default MyApp
+export default withRedux(store)(MyApp);
