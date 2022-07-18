05ebebe2b2338921496d23620ab18e11cb89f7b2
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-07-19 00:32:06 +0900

    [Feat]: 학급 생성 페이지 뼈대 구현

---
+++ styles/Layout.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Layout = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  min-height: 100vh;
+  flex: 1;
+  padding: 1.5rem;
+  width: 100%;
+  max-width: 40rem;
+`;
