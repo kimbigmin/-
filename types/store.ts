c533d07ba6889dc2803e878b2595756c1f20345b
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-07-23 11:10:45 +0900

    [Alert] 타입 추가

--- types/store.ts
+++ types/store.ts
@@ -5,16 +5,29 @@ export type StoreStateDataType = {
     isMobile: boolean,
     userInfo: {
         type: 'S' | 'T' // S(Student), T(Teacher)
-    }
+    },
+    title: string,
+    path: string
 }
 
 export type StoreStateViewType = {
     pageInfo: {
         title: string,
-
-    }
+        type?: 'S' | 'T' // S(Student), T(Teacher)
+    },
+    showAlertMessage: string,
+    showAlertOptions: ShowAlertOptionType
 }
 
 export type StoreStateType = {
+    data: StoreStateDataType,
+    view: StoreStateViewType
+}
 
+export type ShowAlertOptionType = {
+    type: 'alert' | 'confirmAlert' // default = 'alert'
+    titleLabel?: string
+    confirmLabel?: string
+    cancelLabel?: string
+    closeHandler?: Function
 }
