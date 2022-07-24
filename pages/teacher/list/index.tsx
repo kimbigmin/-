7686dca72254027d5288e78a07d07dd5e8626cd4
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-07-24 23:56:17 +0900

    [하드코딩수정]: DB에서 class 정보 불러와서 리스트 렌더링

--- pages/teacher/list/index.tsx
+++ pages/teacher/list/index.tsx
@@ -6,9 +6,47 @@ import { TopBar } from "../../../styles/TopBar";
 import { Layout } from "../../../styles/Layout";
 import styled from "styled-components";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface IClassInfo {
+  [key: string]: string;
+}
 
 /** 선생님이 관리하고 있는 반 리스트 페이지 (메인 페이지에서 측정 결과 입력 누르면 여기로 이동) **/
 const Teacher_ListPage: NextPage = () => {
+  // 관리하고 있는 학급 목록
+  const [classList, setClassList] = useState<IClassInfo[] | undefined>();
+
+  // DB에서 모든 class 리스트 불러와서 선생님이 관리하는 학급만 필터링 후 classList에 할당 => 나중에 서버에서 필터링해서 redux로 관리할 듯...
+  useEffect(() => {
+    const getClassList = async () => {
+      const res = await axios.get(`http://localhost:4000/class`);
+
+      const classList = res.data.filter(
+        (el: any) => el.teacherId === "park123"
+      );
+      setClassList(classList);
+    };
+    getClassList();
+  }, []);
+
+  // 관리하고 있는 학급 리스트 생성하는 함수
+  const createClassList = () => {
+    if (classList) {
+      return classList.map((el, idx) => {
+        return (
+          <Link key={el.id} href={`/teacher/list/${el.classId}`}>
+            <List>
+              <h3>{el.name}</h3>
+              <ArrowForwardIosIcon fontSize="small"></ArrowForwardIosIcon>
+            </List>
+          </Link>
+        );
+      });
+    }
+  };
+
   return (
     <>
       <Head>
@@ -24,24 +62,7 @@ const Teacher_ListPage: NextPage = () => {
           </Link>
         </TopBar>
         <CardBox style={{ maxWidth: "60rem", width: "100%" }}>
-          <Link href={`/teacher/list/${"1-7"}`}>
-            <List>
-              <h3>1학년 7반</h3>
-              <ArrowForwardIosIcon fontSize="small"></ArrowForwardIosIcon>
-            </List>
-          </Link>
-          <Link href={`/teacher/list/${"2-1"}`}>
-            <List>
-              <h3>2학년 1반</h3>
-              <ArrowForwardIosIcon fontSize="small"></ArrowForwardIosIcon>
-            </List>
-          </Link>
-          <Link href={`/teacher/list/${"3-2"}`}>
-            <List>
-              <h3>즐거운 3-2반</h3>
-              <ArrowForwardIosIcon fontSize="small"></ArrowForwardIosIcon>
-            </List>
-          </Link>
+          {createClassList()}
         </CardBox>
       </Layout>
     </>
