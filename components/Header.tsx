6f081865d9ac25fd51672ba2a1be493b13314301
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-07-29 16:46:20 +0900

    [Add]: 헤더 작업 시작

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,25 @@
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import styled from "styled-components";
+
 function Header() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <Layout>
+      <ArrowBackIosIcon></ArrowBackIosIcon>
+      <h1>Health PAPS</h1>
+    </Layout>
   );
 }
+
+const Layout = styled.header`
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  max-height: 5rem;
+
+  h1 {
+    font-size: 1.3rem;
+    justify-self: center;
+  }
+`;
 
 export default Header;
