3336fe0c147a52c6bce6577eae13a887e9df1602
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-08-02 21:37:33 +0900

    [Add]: 남자 측정 등급 구하는 기능 구현 완료

--- core/Standard.ts
+++ core/Standard.ts
@@ -1,4 +1,4 @@
-import { manRange, womanRange } from './Range';
+import Range from './Range';
 
 type Grade =
     | '초4'
@@ -14,69 +14,80 @@ type Grade =
 class Standard {
     // 왕복오래달리기 등급 구하기
     roundRunning(record: string, grade: Grade, gender: string) {
-        return getGradeFromRecord(record, manRange.roundRunning[grade]);
+        return getGradeFromRecord(record, Range.roundRunning.man[grade]);
     }
     // 오래달리기-걷기 등급 구하기
     longRunningAndWalking(record: string, grade: Grade, gender: string) {
         if (grade !== '초4') {
             return getGradeFromRecord(
                 record,
-                manRange.longRunningAndWalking[grade],
+                Range.longRunningAndWalking.man[grade],
             );
         } else {
             return null;
         }
     }
     // 스텝체크 등급 구하기
-    stepCheck(record: string, grade: Grade, gender: string) {
-        if (grade !== '초4') {
-            return getGradeFromRecord(record, manRange.stepCheck[grade]);
-        } else {
-            return null;
+    stepCheck(record: string) {
+        switch (true) {
+            case +record <= 100 && +record >= 76.0:
+                return 'first';
+            case +record <= 75.9 && +record >= 62.0:
+                return 'second';
+            case +record <= 61.9 && +record >= 52.0:
+                return 'third';
+            case +record <= 51.9 && +record >= 47.0:
+                return 'fourth';
+            default:
+                return 'fifth';
         }
     }
     // 앉아윗몸앞으로굽히기 등급 구하기
     bendingForward(record: string, grade: Grade, gender: string) {
         if (grade !== '초4') {
-            return getGradeFromRecord(record, manRange.bendingForward[grade]);
+            return getGradeFromRecord(record, Range.bendingForward.man[grade]);
         } else {
             return null;
         }
     }
     // 종합유연성 등급 구하기
-    totalFlex(record: string, grade: Grade, gender: string) {
-        if (grade !== '초4') {
-            return getGradeFromRecord(
-                record,
-                manRange.longRunningAndWalking[grade],
-            );
-        } else {
-            return null;
+    totalFlex(record: string) {
+        switch (record) {
+            case '8':
+                return 'first';
+            case '7':
+                return 'second';
+            case '6':
+                return 'third';
+            case '5':
+                return 'fourth';
+            default:
+                return 'fifth';
         }
     }
     // 팔굽혀펴기 등급 구하기
     pushUps(record: string, grade: Grade, gender: string) {
         if (grade !== '초4' && grade !== '초5' && grade !== '초6') {
-            return getGradeFromRecord(record, manRange.pushUps[grade]);
+            return getGradeFromRecord(record, Range.pushUps.man[grade]);
         } else {
             return null;
         }
     }
     // 윗몸말아올리기 등급 구하기
     upperBody(record: string, grade: Grade, gender: string) {
-        return getGradeFromRecord(record, manRange.upperBody[grade]);
+        return getGradeFromRecord(record, Range.upperBody.man[grade]);
     }
     // 악력 등급 구하기
     grip(record: string, grade: Grade, gender: string) {
-        return getGradeFromRecord(record, manRange.grip[grade]);
+        return getGradeFromRecord(record, Range.grip.man[grade]);
     }
     // 50m 달리기 등급 구하기
     running50m(record: string, grade: Grade, gender: string) {
-        return getGradeFromRecord(record, manRange.running50m[grade]);
+        return getGradeFromRecord(record, Range.running50m.man[grade]);
     }
     // 제자리멀리뛰기 등급 구하기
     longJump(record: string, grade: Grade, gender: string) {
-        return getGradeFromRecord(record, manRange.longJump[grade]);
+        return getGradeFromRecord(record, Range.longJump.man[grade]);
     }
 }
 
@@ -88,14 +99,14 @@ const getGradeFromRecord = (
     record: string | number,
     range: number[],
     reverse?: boolean,
-) => {
+): string => {
     record = +record;
 
     if (reverse) {
         switch (true) {
             case (record <= range[1] && record >= range[0]) ||
                 record < range[0]:
-                return 'first';
+                return 'first'; // first: 1등급 second: 2등급 .....
             case record <= range[3] && record >= range[2]:
                 return 'second';
             case record <= range[5] && record >= range[4]:
