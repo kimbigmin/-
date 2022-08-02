c20eacd8e601fffb4d4f325d4d9d40d6a54e30d5
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-08-02 16:50:33 +0900

    [Add]: hp 스키마 및 API 구성

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,261 +1,639 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createBlog = /* GraphQL */ `
-  mutation CreateBlog(
-    $input: CreateBlogInput!
-    $condition: ModelBlogConditionInput
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    createBlog(input: $input, condition: $condition) {
+    createUser(input: $input, condition: $condition) {
       id
+      isTeacher
       name
-      posts {
-        items {
-          id
-          title
-          createdAt
-          updatedAt
-          blogPostsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const updateBlog = /* GraphQL */ `
-  mutation UpdateBlog(
-    $input: UpdateBlogInput!
-    $condition: ModelBlogConditionInput
-  ) {
-    updateBlog(input: $input, condition: $condition) {
-      id
-      name
-      posts {
-        items {
-          id
-          title
-          createdAt
-          updatedAt
-          blogPostsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const deleteBlog = /* GraphQL */ `
-  mutation DeleteBlog(
-    $input: DeleteBlogInput!
-    $condition: ModelBlogConditionInput
-  ) {
-    deleteBlog(input: $input, condition: $condition) {
-      id
-      name
-      posts {
-        items {
-          id
-          title
-          createdAt
-          updatedAt
-          blogPostsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const createPost = /* GraphQL */ `
-  mutation CreatePost(
-    $input: CreatePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    createPost(input: $input, condition: $condition) {
-      id
-      title
-      blog {
+      schoolName
+      email
+      image
+      classNum
+      sex
+      age
+      grade
+      school
+      record {
         id
+        endurance
+        flexibility
+        strength
+        speediness
+        createdAt
+        updatedAt
+      }
+      belong {
+        id
+        teacher {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
         name
-        posts {
+        school
+        grade
+        endurance
+        flexibility
+        strength
+        speediness
+        pending {
+          nextToken
+        }
+        students {
           nextToken
         }
         createdAt
         updatedAt
+        userManagingId
+        classTeacherId
       }
-      comments {
+      managing {
         items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      blogPostsId
-    }
-  }
-`;
-export const updatePost = /* GraphQL */ `
-  mutation UpdatePost(
-    $input: UpdatePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    updatePost(input: $input, condition: $condition) {
-      id
-      title
-      blog {
-        id
-        name
-        posts {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      comments {
-        items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      blogPostsId
-    }
-  }
-`;
-export const deletePost = /* GraphQL */ `
-  mutation DeletePost(
-    $input: DeletePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    deletePost(input: $input, condition: $condition) {
-      id
-      title
-      blog {
-        id
-        name
-        posts {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      comments {
-        items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      blogPostsId
-    }
-  }
-`;
-export const createComment = /* GraphQL */ `
-  mutation CreateComment(
-    $input: CreateCommentInput!
-    $condition: ModelCommentConditionInput
-  ) {
-    createComment(input: $input, condition: $condition) {
-      id
-      post {
-        id
-        title
-        blog {
           id
           name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
           createdAt
           updatedAt
+          userManagingId
+          classTeacherId
         }
-        comments {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        blogPostsId
+        nextToken
       }
-      content
       createdAt
       updatedAt
-      postCommentsId
+      classPendingId
+      classStudentsId
+      userBelongId
     }
   }
 `;
-export const updateComment = /* GraphQL */ `
-  mutation UpdateComment(
-    $input: UpdateCommentInput!
-    $condition: ModelCommentConditionInput
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    updateComment(input: $input, condition: $condition) {
+    updateUser(input: $input, condition: $condition) {
       id
-      post {
+      isTeacher
+      name
+      schoolName
+      email
+      image
+      classNum
+      sex
+      age
+      grade
+      school
+      record {
         id
-        title
-        blog {
+        endurance
+        flexibility
+        strength
+        speediness
+        createdAt
+        updatedAt
+      }
+      belong {
+        id
+        teacher {
           id
+          isTeacher
           name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
           createdAt
           updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
         }
-        comments {
+        name
+        school
+        grade
+        endurance
+        flexibility
+        strength
+        speediness
+        pending {
+          nextToken
+        }
+        students {
           nextToken
         }
         createdAt
         updatedAt
-        blogPostsId
+        userManagingId
+        classTeacherId
       }
-      content
+      managing {
+        items {
+          id
+          name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+          userManagingId
+          classTeacherId
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      postCommentsId
+      classPendingId
+      classStudentsId
+      userBelongId
     }
   }
 `;
-export const deleteComment = /* GraphQL */ `
-  mutation DeleteComment(
-    $input: DeleteCommentInput!
-    $condition: ModelCommentConditionInput
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    deleteComment(input: $input, condition: $condition) {
+    deleteUser(input: $input, condition: $condition) {
       id
-      post {
+      isTeacher
+      name
+      schoolName
+      email
+      image
+      classNum
+      sex
+      age
+      grade
+      school
+      record {
         id
-        title
-        blog {
+        endurance
+        flexibility
+        strength
+        speediness
+        createdAt
+        updatedAt
+      }
+      belong {
+        id
+        teacher {
           id
+          isTeacher
           name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
           createdAt
           updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
         }
-        comments {
+        name
+        school
+        grade
+        endurance
+        flexibility
+        strength
+        speediness
+        pending {
+          nextToken
+        }
+        students {
           nextToken
         }
         createdAt
         updatedAt
-        blogPostsId
+        userManagingId
+        classTeacherId
       }
-      content
+      managing {
+        items {
+          id
+          name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+          userManagingId
+          classTeacherId
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      postCommentsId
+      classPendingId
+      classStudentsId
+      userBelongId
+    }
+  }
+`;
+export const createRecord = /* GraphQL */ `
+  mutation CreateRecord(
+    $input: CreateRecordInput!
+    $condition: ModelRecordConditionInput
+  ) {
+    createRecord(input: $input, condition: $condition) {
+      id
+      endurance
+      flexibility
+      strength
+      speediness
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateRecord = /* GraphQL */ `
+  mutation UpdateRecord(
+    $input: UpdateRecordInput!
+    $condition: ModelRecordConditionInput
+  ) {
+    updateRecord(input: $input, condition: $condition) {
+      id
+      endurance
+      flexibility
+      strength
+      speediness
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteRecord = /* GraphQL */ `
+  mutation DeleteRecord(
+    $input: DeleteRecordInput!
+    $condition: ModelRecordConditionInput
+  ) {
+    deleteRecord(input: $input, condition: $condition) {
+      id
+      endurance
+      flexibility
+      strength
+      speediness
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createClass = /* GraphQL */ `
+  mutation CreateClass(
+    $input: CreateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    createClass(input: $input, condition: $condition) {
+      id
+      teacher {
+        id
+        isTeacher
+        name
+        schoolName
+        email
+        image
+        classNum
+        sex
+        age
+        grade
+        school
+        record {
+          id
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+        }
+        belong {
+          id
+          name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+          userManagingId
+          classTeacherId
+        }
+        managing {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        classPendingId
+        classStudentsId
+        userBelongId
+      }
+      name
+      school
+      grade
+      endurance
+      flexibility
+      strength
+      speediness
+      pending {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      students {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      userManagingId
+      classTeacherId
+    }
+  }
+`;
+export const updateClass = /* GraphQL */ `
+  mutation UpdateClass(
+    $input: UpdateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    updateClass(input: $input, condition: $condition) {
+      id
+      teacher {
+        id
+        isTeacher
+        name
+        schoolName
+        email
+        image
+        classNum
+        sex
+        age
+        grade
+        school
+        record {
+          id
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+        }
+        belong {
+          id
+          name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+          userManagingId
+          classTeacherId
+        }
+        managing {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        classPendingId
+        classStudentsId
+        userBelongId
+      }
+      name
+      school
+      grade
+      endurance
+      flexibility
+      strength
+      speediness
+      pending {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      students {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      userManagingId
+      classTeacherId
+    }
+  }
+`;
+export const deleteClass = /* GraphQL */ `
+  mutation DeleteClass(
+    $input: DeleteClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    deleteClass(input: $input, condition: $condition) {
+      id
+      teacher {
+        id
+        isTeacher
+        name
+        schoolName
+        email
+        image
+        classNum
+        sex
+        age
+        grade
+        school
+        record {
+          id
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+        }
+        belong {
+          id
+          name
+          school
+          grade
+          endurance
+          flexibility
+          strength
+          speediness
+          createdAt
+          updatedAt
+          userManagingId
+          classTeacherId
+        }
+        managing {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        classPendingId
+        classStudentsId
+        userBelongId
+      }
+      name
+      school
+      grade
+      endurance
+      flexibility
+      strength
+      speediness
+      pending {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      students {
+        items {
+          id
+          isTeacher
+          name
+          schoolName
+          email
+          image
+          classNum
+          sex
+          age
+          grade
+          school
+          createdAt
+          updatedAt
+          classPendingId
+          classStudentsId
+          userBelongId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      userManagingId
+      classTeacherId
     }
   }
 `;
